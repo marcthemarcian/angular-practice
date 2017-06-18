8f5c4ac4748f7a18ff439b9d7ea43ad1ce15fc0a
Marc Uy <marcthemarcian@gmail.com>
2017-06-18 10:33:36 +0800

    completed part 5 of tour of heroes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,11 @@
 import { Component } from '@angular/core';
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
-import { OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'my-app',
+  templateUrl: `./templates/app.component.html`,
   styleUrls: ['./app.component.css'],
-  providers: [HeroService]
 })
 
 export class AppComponent {
   title = 'Tour of Heroes';
-  selectedHero: Hero;
-  heroes: Hero[];
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  };
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  };
-  constructor(private heroService: HeroService) {
-
-  };
-  ngOnInit(): void {
-    this.getHeroes();
-  }
 }
